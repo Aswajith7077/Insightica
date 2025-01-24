d4e025b457daf37791d785939025572f698bcef6
Aswajith S <aswajiths777@gmail.com>
2025-01-24 11:07:01 +0530

    path .jsx removal

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState } from "react";
+// import { useState } from "react";
 import { AuthProvider } from "@/auth/AuthContext";
 
-// import Documentation from "@/components/dashboard/Documentation.jsx";
-import Dashboard from "@/components/dashboard/DashBoard.jsx";
-import ComingSoon from "@/components/ComingSoon.jsx";
-import Login from "@/components/Login.jsx";
-import SignIn from "@/components/SignUp.jsx";
+import Documentation from "@/components/dashboard/Documentation";
+import Dashboard from "@/components/dashboard/DashBoard";
+import ComingSoon from "@/components/ComingSoon";
+import Login from "@/components/Login";
+import SignIn from "@/components/SignUp";
 import NavBar from "@/components/dashboard/NavBar";
 import Tools from "@/components/dashboard/Tools";
 import Footer from "@/components/dashboard/Footer";
@@ -16,9 +16,9 @@ import Support from "@/components/dashboard/Support";
 import { ResetProvider } from "@/auth/ResetContext";
 
 function App() {
-  const [accessToken, setAccessToken] = useState("");
-  const [responseToken, setRefreshToken] = useState("");
-  const [isLogged, setIsLogged] = useState(undefined);
+  // const [accessToken, setAccessToken] = useState("");
+  // const [responseToken, setRefreshToken] = useState("");
+  // const [isLogged, setIsLogged] = useState(undefined);
   return (
     <AuthProvider>
       <BrowserRouter>
@@ -29,7 +29,7 @@ function App() {
             element={
               <div>
                 <NavBar />
-                {/* <Documentation /> */}
+                <Documentation />
               </div>
             }
           />
